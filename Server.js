e6c85a2dee69084ac6c6d43855e030d5d6e55d96
Your agacik828 <agacik828@wp.plgit config --global user.name Your>
2021-11-04 18:19:18 +0100

    dodanie pliku

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const requestListener = function(req, res)
 {
 res.writeHead(200)
-res.end('Hello Agata');    tu sobie zmieniam nazwe
+res.end('Hello Agata');
 }
 const server = http.createServer(requestListener);
 server.listen(8080);
